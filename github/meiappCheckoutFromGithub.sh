14b5a02419367e7e821d9e2dd3ec99630fab3135
wangjstu <wangjstu@gmail.com>
2018-06-24 17:14:04 +0800

    add github/meiappCheckoutFromGithub.sh for update github code

--- github/meiappCheckoutFromGithub.sh
+++ github/meiappCheckoutFromGithub.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+
+#check user is not root
+[ "$(id -u)" == "0" ] && echo "Error: You must not be root to run this script." && exit 1
+
 function RUNCMD() {
 	echo "[$(date +'%Y-%m-%d %H:%M:%S:%N')][notice] $*"
-	echo -e "\033[41;30m ####################EVAL LOG####################### \033[0m"
+	echo -e "\033[41;33m ####################EVAL LOG####################### \033[0m"
 	eval $@
-	echo -e "\033[41;30m ####################EVAL LOG####################### \033[0m"
+	echo -e "\033[41;33m ####################EVAL LOG####################### \033[0m"
 }
 
 function NOTICE() {
 	echo >/dev/null && echo "[$(date +'%Y-%m-%d %H:%M:%S:%N')][notice] $*"
+}
+
+function ERROR() {
+	echo >/dev/null && echo -e "\033[41;30m [$(date +'%Y-%m-%d %H:%M:%S:%N')][error] $* \033[0m"
 }
 
 
@@ -15,6 +23,40 @@ function Git_Pull() {
 	RUNCMD "git pull origin master"
 }
 
-NOTICE "----------------------BEGIND Git_Pull-----------------"
-Git_Pull
-NOTICE "-----------------------END Git_Pull-------------------"
+function Git_Push() {
+	echo $#
+	if [ $# -ne 2 ]; then
+		ERROR "Usage: sh meiappCheckoutFromGithub.sh Git_Push commit_comments!"
+		exit 1;
+	else
+		RUNCMD "git pull && git add --all && git commit -m \"$2\" && git push origin master"
+	fi	
+}
+
+NOTICE "----------------------BEGIND-----------------"
+
+case $# in
+	1)
+	{
+		NOTICE "run in Git_Pull"
+		Git_Pull
+	}	
+	;;
+	2)
+	{
+		NOTICE "run in Git_Push"
+		Git_Push "$@"
+	}
+	;;
+	*)
+	{
+		NOTICE "run nothing"
+		echo "please Usage:"
+		echo "$0 Git_Push Git_Push_Comment"
+		echo "$0 Git_Pull"
+	}
+esac
+
+NOTICE "-----------------------END-------------------"
+
+exit 0
